4c55798f956257c7e286ffcf484a4c5c9faaf241
Aline <rdg.aline@yahoo.com.br>
2021-04-20 14:15:48 -0300

    collection in collections

---
+++ modulo 2 - Beginning C# Collections/Cap.8-Creating Collections of Collections/Countries/Countries/CsvReader.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Countries
+{
+    class CsvReader
+    {
+        #region Field
+        //var irá informar o caminho da fonte de dados
+        private string _csvFilePath;
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// This method expect the CSV ile path
+        /// </summary>
+        /// <param name="csvFilePath"></param>
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+
+        /// <summary>
+        /// Este método será um dicionario com a lista das regiões da cada pais
+        /// <para> 1 ler o arquivo</para>
+        ///    <para>     2 verifico se a chave existe no dicionario</para>
+        ///     <para>      - ContainsKey: true = >(região está no dicionario) --> adiciono novo pais ao dicionario </para>
+        ///      <para>     - ContainsKey: false => (região não está no dicionario)--> crio lista de paises para a região e add ao Dicionario
+        /// </para>
+        /// </summary>
+        /// <param name="nCountries"></param>
+        /// <returns></returns>
+        public Dictionary<string, List<Country>> ReadAllCountries()
+        {
+             var countries = new Dictionary<string, List<Country>> ();
+
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                // read header line
+                sr.ReadLine();
+
+                string csvLine;
+                while ((csvLine = sr.ReadLine()) != null)
+                {
+                    Country country = ReadCountryFromCsvLine(csvLine);
+                    if (countries.ContainsKey(country.Region))
+                    {
+                        countries[country.Region].Add(country);
+                    }
+                    else
+                    {
+                        List<Country> countriesInRegion = new List<Country>() { country };
+                        countries.Add(country.Region, countriesInRegion);
+                    }
+                }
+            }
+            return countries;
+        }
+
+
+        /// <summary>
+        /// Este método recebe como argumento  o dado de cada linha do arquivo CSV referente a um país
+        /// </summary>
+        /// <param name="csvLine"></param>
+        /// <returns></returns>
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+            string name, code, region, text;
+
+            switch(parts.Length)
+            {
+                case 4: // paises com um único nome
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    text = parts[3];
+                    break;
+                case 5: //paises com nome  composto 
+                    name = parts[0] + ","+ parts[1];
+                    name = name.Replace("\"", null).Trim();//remove as aspas-duplas
+                    code = parts[2];
+                    region = parts[3];
+                    text = parts[4];
+                    break;
+                default:
+                    throw new Exception($"Can't parse country from file: {csvLine}");
+            }
+
+            //if population be unknown will return 0
+            int.TryParse(text,out int population);
+            return new Country(name, code, region, population);
+
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ modulo 2 - Beginning C# Collections/Cap.8-Creating Collections of Collections/Countries/Countries/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Countries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Aline Rodrigues\source\repos\Countries\Pop by Largest Final.csv"; // definir o caminho do arquivo CSV- precisa colocar o @
+
+            CsvReader reader = new CsvReader(filePath);
+
+            //fill the Country collection calling ReadAllCountries method
+            Dictionary<string, List<Country>> countries = reader.ReadAllCountries();
+            
+            // Display title of app.
+            Console.WriteLine("Country list");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to choose an option.
+            while (true)
+            {
+               Console.WriteLine("Choose an option from the following list:" +
+               "\n 1 - Check the country per code " +
+               "\n 2 - List all countries'name and codes" +
+               "\n 3 - Display countries per region" +
+               "\n 4 - Exit application \n ");
+                
+
+                var userOption = Console.ReadLine();
+                if (userOption == "1")
+                {
+
+                    /*Console.WriteLine("Which country code do you want to look up? ");
+                    string userInput = Console.ReadLine().ToUpper();
+
+                  
+
+                    bool gotCountry = countries.TryGetValue(userInput, out Country  country);
+
+                    if (!gotCountry)
+                        Console.WriteLine($"Sorry, there is no country with code, {userInput}");
+                    else
+                        Console.WriteLine($"{country.Name}, which code is {country.Code}, has population {PopulationFormatter.FormatPopulation(country.Population)} and belong to {country.Region} ");
+
+                    */
+                    Console.WriteLine("option1");
+                }
+                else if (userOption == "2")
+                {
+                   // foreach (Country nextCountry in countries.Values) 
+                    { 
+                      // Console.WriteLine($"Code: {nextCountry.Code} - Name: {nextCountry.Name}");
+                    }
+                }
+                else if (userOption == "3")
+                {
+                    //Display all the regions
+                    foreach (string region in countries.Keys)
+                        Console.WriteLine(region);
+
+                    Console.Write("\n Which of the above regions do you want?");
+                    string chosenRegion = Console.ReadLine();
+               
+
+                    Console.WriteLine(chosenRegion);
+                    if (countries.ContainsKey(chosenRegion))
+                    {
+                        // display 10 highest population countries in the selected region
+                        foreach (Country country in countries[chosenRegion].Take(10))
+                            Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(5)}: {country.Name}");
+                    }
+                    else
+                        Console.WriteLine("That is not a valid region");
+
+                }
+                else if (userOption == "4")
+                {
+                    Console.WriteLine("See you next time");
+                    break;
+                }
+                else
+                    Console.WriteLine("This is not a valid option, please choose from 1 to 4");
+            }
+        }
+    }
+}
