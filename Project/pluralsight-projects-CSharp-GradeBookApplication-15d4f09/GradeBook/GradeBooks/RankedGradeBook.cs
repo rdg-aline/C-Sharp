0e845c1e484d396741b84ec4d19b1907ce883939
Aline <rdg.aline@yahoo.com.br>
2021-04-23 18:40:57 -0300

    Creating New GradeBookTypes

---
+++ Project/pluralsight-projects-CSharp-GradeBookApplication-15d4f09/GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        /// <summary>
+        /// This constructor is required to invoke a constructor from BaseGradeBook.
+        /// </summary>
+        /// <param name="name"></param>
+        public RankedGradeBook(string name): base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
