7f37d0c108dfcc042a4d91a8b97d4bd553dabcad
Aline <rdg.aline@yahoo.com.br>
2021-04-27 19:29:44 -0300

    IEnumerable

---
+++ Intermediate/LINQ Fundamentals/Cap.2-Introduction/Introduction - with and without LINQ/Introduction/Features/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           IEnumerable<Employee> developers = new Employee[]
+          // Employee[] developers = new Employee[]
+           {
+                new Employee { Id = 1, Name= "Scott" },
+                new Employee { Id = 2, Name= "Chris" }
+           };
+
+            IEnumerable <Employee> sales = new List<Employee>()
+            //List <Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            //foreach statment  behide the scene 
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+            /*
+                foreach (var person in sales)
+                {
+                    Console.WriteLine(person.Name);
+                }
+             */
+        }
+    }
+}
